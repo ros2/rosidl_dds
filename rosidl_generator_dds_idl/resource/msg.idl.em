45f55cdad995e1be425b91df825e695fa72dea43
Michel Hidalgo <hid.michel@gmail.com>
2018-11-29 16:18:01 -0800

    Updates IDL templates for messages.

--- rosidl_generator_dds_idl/resource/msg.idl.em
+++ rosidl_generator_dds_idl/resource/msg.idl.em
@@ -15,162 +15,39 @@ module dds_ {
 
 @[if message.constants]@
 module @{message.structure.type.name}@@(CONSTANT_MODULE_SUFFIX) {
-@[  for constantc in message.constants]@
-  @
-@[if isinstance(constant.type, BasicType)]@
-@(constant.type)@
-@[elif isinstance(constant.type, NestedType)]@
-XXX array[] need to go behind the constant name though
-@[end for]@
- @
-@(constant.name)@
- = @
-@[    if isinstance(constant.type, BasicType)]@
-@[      if constant.type.type == 'boolean']@
-@('TRUE' if constant.value else 'FALSE')@
-@(constant.value)@
-@[      elif constant.type.type == 'char']@
-'@(constant.value)'@
-@[      else XXX-only-numerics?XXX]@
-@[      end for]@
-@[    elif isinstance(constant.type, String)]@
-"@(constant.value)"
-@[    elif isinstance(constant.type, NestedType)]@
-XXX array[] need to go behind the constant name though
-@[    end for]@
-;
-@[  end for]@
+@[  for constant in message.constants]@
+const @(idl_base_type(constant.type)) @(constant.name) = @(idl_literal(constant.value));
+@[  end for]
 }
 @[end if]@
-@[for m in members]@
-@[  if isinstance(member.type, BasicType)]@
-@(member.type)@
-@[  elif isinstance(member.type, Sequence)]@
-sequence<@(member.type.basetype)@
-@[    if isinstance(member.type, BoundedSequence)]@
+
+struct @(message.structure.type.name)_ {
+@[for member in message.structure.members]@
+@[  if isinstance(member.type, NestedType)]@
+@[    if isinstance(member.type, Array)]@
+@(idl_base_type(member.type.basetype)) @(member.name)[@(member.type.size)];
+@[    elif isinstance(member.type, Sequence)]@
+sequence<@(idl_base_type(member.type.basetype))@
+@[      if isinstance(member.type, BoundedSequence)]@
 , @(member.type.upper_bound)@
-@[    end if]@
->@
-@[  elif XXX]@
-@# XXX typedefs for arrays
-XXX@
-@[end if]@
- @
-@(member.name);
-@[end for]@
-
-@##############################################################################
-@##############################################################################
-@##############################################################################
-
-// generated from rosidl_generator_dds_idl/resource/msg.idl.em
-
-@###############################################
-@#
-@# ROS interface to DDS interface converter
-@#
-@# EmPy template for generating <msg>.idl files
-@#
-@###############################################
-@# Start of Template
-@#
-@# Context:
-@#  - spec (rosidl_parser.MessageSpecification)
-@#    Parsed specification of the .msg file
-@#  - subfolder (string)
-@#    The subfolder / subnamespace of the message
-@#    Could be 'msg', 'srv' or 'action'
-@#  - deps_subfolder (string)
-@#    The subfolder / subnamespace of the message dependencies
-@#    Could be 'msg', 'srv' or 'action'
-@#  - subfolders (list of strings)
-@#    The subfolders under the package name
-@#    in which the type gets defined which are not part of the namespace
-@#  - get_include_directives (function)
-@#  - msg_type_to_idl (function)
-@###############################################
-@
-@{
-from rosidl_generator_cpp import escape_string
-from rosidl_generator_dds_idl import MSG_TYPE_TO_IDL
-}@
-@
-#ifndef __@(spec.base_type.pkg_name)__@(subfolder)__@(spec.base_type.type)__idl__
-#define __@(spec.base_type.pkg_name)__@(subfolder)__@(spec.base_type.type)__idl__
-
-@#############################
-@# Include dependency messages
-@#############################
-@[  for line in get_include_directives(spec, [deps_subfolder] + subfolders)]@
-@(line)
-@[  end for]@
-
-module @(spec.base_type.pkg_name)
-{
-
-module @(subfolder)
-{
-
-module dds_
-{
-
-@##################
-@# Define constants
-@##################
-@# Constants
-@[for constant in spec.constants]@
-  const @(MSG_TYPE_TO_IDL[constant.type]) @(spec.base_type.type)__@(constant.name) =
-@[  if constant.type == 'bool']@
-    @('TRUE' if constant.value else 'FALSE');
-@[  elif constant.type == 'char']@
-    '\@(constant.value)';
-@[  elif constant.type == 'int8']@
-    @(constant.value if constant.value >= 0 else (constant.value + 256));
-@[  elif constant.type == 'string']@
-    "@(escape_string(constant.value))";
-@[  else]@
-    @(constant.value);
-@[  end if]@
-@[end for]
-
-@{
-typedefs = set([])
-for field in spec.fields:
-  idl_typedef, idl_typedef_var, _ = msg_type_to_idl(field.type, deps_subfolder)
-  if idl_typedef and idl_typedef_var and (idl_typedef, idl_typedef_var) not in typedefs:
-    print('%s %s__%s__%s' % (idl_typedef, spec.base_type.pkg_name, spec.base_type.type, idl_typedef_var))
-    typedefs.add((idl_typedef, idl_typedef_var))
-}@
-
-@################################
-@# Message struct with all fields
-@################################
-struct @(spec.base_type.type)_
-{
-
-@[if spec.fields]@
-@[  for field in spec.fields]@
-@{    idl_typedef, idl_typedef_var, idl_type = msg_type_to_idl(field.type, deps_subfolder)}@
-@[    if idl_typedef and idl_typedef_var]@
-@(      spec.base_type.pkg_name)__@(spec.base_type.type)__@(idl_type) @(field.name)_;
+@[      end if]@
+> @(member.name);
+@(member.name)[@(member.type.size)];
 @[    else]@
-  @(idl_type) @(field.name)_;
-@[    end if]@
-@[  end for]@
-@[else]@
-  boolean _dummy;
-@[end if]@
 
-};  // struct @(spec.base_type.type)_
+@[    end if]
+@[  elif isinstance(member.type, BaseType)]@
+@(idl_typename(member.type)) @(member.name);
+@[  else]@
 
-@[for line in get_post_struct_lines(spec)]@
-@(line)
+@[  end if]@
 @[end for]@
+
+};
 
 };  // module dds_
 
-};  // module @(subfolder)
+@[for ns in reversed(message.structure.type.namespaces)]@
+};  // module @(ns) {
 
-};  // module @(spec.base_type.pkg_name)
-
-#endif  // __@(spec.base_type.pkg_name)__@(subfolder)__@(spec.base_type.type)__idl__
+@[end for]@
--- rosidl_generator_dds_idl/rosidl_generator_dds_idl/__init__.py
+++ rosidl_generator_dds_idl/rosidl_generator_dds_idl/__init__.py
@@ -12,154 +12,63 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
+from rosidl_cmake import generate_files
+from rosidl_parser import BaseType
+from rosidl_parser import BaseString
+from rosidl_parser import BasicType
+from rosidl_parser import NamespacedType
+from rosidl_parser import String
+from rosidl_parser import WString
 
-from rosidl_cmake import expand_template
-from rosidl_cmake import get_newest_modification_time
-from rosidl_cmake import read_generator_arguments
-from rosidl_parser.definition import IdlLocator
-from rosidl_parser.parser import parse_idl_file
 
-
-def convert_dds_idl(generator_arguments_file, subfolders, extension_module_name):
-    args = read_generator_arguments(generator_arguments_file)
-
-    template_file = os.path.join(args['template_dir'], 'idl.idl.em')
-    assert os.path.exists(template_file), \
-        "The template '%s' does not exist" % template_file
-
-    # look for extensions for the default functions
-    # TODO: update as needed
-    functions = {
-        'get_include_directives': get_include_directives,
-        'get_post_struct_lines': get_post_struct_lines,
-        'msg_type_to_idl': msg_type_to_idl,
+def generate_dds_idl(generator_arguments_file):
+    mapping = {
+        'idl.idl.em': '%s.idl'
     }
-    if extension_module_name is not None:
-        pkg = __import__(extension_module_name)
-        module_name = extension_module_name.rsplit('.', 1)[1]
-        if hasattr(pkg, module_name):
-            module = getattr(pkg, module_name)
-            for function_name in functions.keys():
-                if hasattr(module, function_name):
-                    functions[function_name] = \
-                        getattr(module, function_name)
-
-    latest_target_timestamp = get_newest_modification_time(args['target_dependencies'])
-
-    for idl_tuples in args.get('idl_tuples', []):
-        idl_parts = idl_tuple.rsplit(':', 1)
-        assert len(idl_parts) == 2
-
-        locator = IdlLocator(*idl_parts)
-        idl_rel_path = pathlib.Path(idl_parts[1])
-        try:
-            idl_file = parse_idl_file(
-                locator, png_file=os.path.join(
-                    args['output_dir'], str(idl_rel_path.parent),
-                    idl_rel_path.stem) + '.png')
-
-            generated_file = os.path.join(
-                args['output_dir'], str(idl_rel_path.parent),
-                generated_filename %
-                convert_camel_case_to_lower_case_underscore(idl_rel_path.stem))
-            data = {
-                'package_name': args['package_name'],
-                'interface_path': idl_rel_path,
-                'content': idl_file.content
-            }
-            data.update(functions)
-
-            expand_template(
-                os.path.basename(template_file), data,
-                generated_file, minimum_timestamp=latest_target_timestamp,
-                template_basepath=template_basepath)
-
-        except Exception as e:
-            print(
-                'Error processing idl file: ' +
-                str(locator.get_absolute_path()), file=sys.stderr)
-            raise(e)
+    functions = {
+        'idl_typename': idl_typename,
+        'idl_literal': idl_literal
+    }
+    generate_files(generator_arguments_file, mapping, functions)
     return 0
 
 
-MSG_TYPE_TO_IDL = {
-    'bool': 'boolean',
-    'byte': 'octet',
-    'char': 'char',
-    'int8': 'octet',
-    'uint8': 'octet',
-    'int16': 'short',
-    'uint16': 'unsigned short',
-    'int32': 'long',
-    'uint32': 'unsigned long',
-    'int64': 'long long',
-    'uint64': 'unsigned long long',
-    'float32': 'float',
-    'float64': 'double',
-    'string': 'string',
-}
-
-
 # used by the template
-def get_include_directives(spec, subfolders):
-    include_directives = set()
-    for field in spec.fields:
-        if field.type.is_primitive_type():
-            continue
-        include_directive = '#include "%s/%s_.idl"' % \
-            ('/'.join([field.type.pkg_name] + subfolders), field.type.type)
-        include_directives.add(include_directive)
-    return sorted(include_directives)
-
-
-# used by the template
-def get_post_struct_lines(spec):
-    return []
-
-
-# used by the template
-def msg_type_to_idl(type_, ns):
-    """
-    Convert a message type into the DDS declaration.
-
-    Example input: uint32, std_msgs/String
-    Example output: uint32_t, std_msgs::String_<ContainerAllocator>
-
-    @param type: The message type
-    @type type: rosidl_parser.Type
-    """
-    string_upper_bound = None
-    if type_.is_primitive_type():
-        idl_type = MSG_TYPE_TO_IDL[type_.type]
-        if type_.type == 'string' and type_.string_upper_bound is not None:
-            string_upper_bound = type_.string_upper_bound
-    else:
-        idl_type = '%s::%s::dds_::%s_' % (type_.pkg_name, ns, type_.type)
-    return _msg_type_to_idl(type_, idl_type, string_upper_bound=string_upper_bound)
-
-
-def _msg_type_to_idl(type_, idl_type, string_upper_bound=None):
-    if type_.is_array:
-        if type_.array_size is None or type_.is_upper_bound:
-            sequence_type = idl_type
-            if string_upper_bound is not None:
-                sequence_type += '<%s>' % string_upper_bound
-            if type_.is_upper_bound:
-                sequence_type += ', %u' % type_.array_size
-            return [
-                '', '',
-                'sequence<%s%s>' % (sequence_type, ' ' if sequence_type.endswith('>') else '')]
+def idl_typename(type_):
+    assert(isinstance(type_, BaseType))
+    if isinstance(type_, BasicType):
+        typename = type_.type
+    elif isinstance(type_, BaseString):
+        if isinstance(type_, String):
+            typename = 'string'
+        elif isinstance(type_, WString):
+            typename = 'wstring'
         else:
-            typename = '%s_array_%s' % \
-                (idl_type.replace(' ', '_').replace('::', '__'), type_.array_size)
-            return [
-                'typedef %s' % idl_type,
-                '%s[%s];' % (typename, type_.array_size),
-                '%s' % typename
-            ]
-    elif type_.string_upper_bound is not None and \
-            type_.is_primitive_type() and type_.type == 'string':
-        return ['', '', '%s<%u>' % (idl_type, type_.string_upper_bound)]
+            assert False, 'Unknown string type'
+        if type_.maximum_size is not None:
+            typename += '<%d>' % (type_.maximum_size)
+    elif isinstance(type_, NamespacedType):
+        typename = '::'.join(type_.namespaces + ['dds_', type_.name + '_'])
     else:
-        return ['', '', idl_type]
+        assert False, 'Unknown base type'
+    return typename
+
+
+def idl_literal(type_, value):
+    assert(isinstance(type_, BaseType))
+    if isinstance(type_, BasicType):
+        if type_.type == 'boolean':
+            literal = '''TRUE''' if value else '''FALSE'''
+        elif type_.type == 'char':
+            literal = '''%s''' % value
+        elif type_.type in ('float', 'double'):
+            literal = '%f' % value
+        else:
+            literal = '%d' % value
+    elif isinstance(type_, String):
+        literal = '"%s"' % value
+    elif isinstance(type_, WString):
+        literal = 'L"%s"' % value
+    else:
+        assert False, 'Unknown base type'
+    return literal
